1bde6331d00f37367bb5c617e9902efbd7d49ea6
alldine apps <alldineapp@gmail.com>
2026-03-19 12:32:08 +0530

    update: frontend connected to new AWS backend

--- flutter_app/lib/config/api_config_constants.dart
+++ flutter_app/lib/config/api_config_constants.dart
@@ -1,4 +1,4 @@
 /// Live/production API only (no localhost). Change when you deploy to a new server or use a domain.
 /// Example with domain: https://api.yourdomain.com/
 /// Live backend (production)
-const String kProductionApiUrl = 'http://13.233.133.213:3000/api/';
+const String kProductionApiUrl = 'http://65.0.93.143:5000/api/';
